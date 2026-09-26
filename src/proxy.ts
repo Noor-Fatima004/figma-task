3eea12d1465d0b1cebe0b722fec810ef6dafb5d3
mehak noor <mehak447464@gmail.com>
2026-09-26 18:14:44 +0500

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -15,22 +15,38 @@ export function proxy(req: NextRequest) {
   const token = req.cookies.get("token")?.value;
   const { pathname } = req.nextUrl;
   const user = getUser(token);
-  console.log("PROXY:", { pathname, hasToken: !!token, user });
+  
   const isAuthPage = pathname === "/login" || pathname === "/signup";
   const isHomePage = pathname === "/";
+  const isAdminRoute = pathname.startsWith("/admin");
+  if (!user) {
+    // Trying to access protected pages without login
+    if (isHomePage || isAdminRoute) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+    return NextResponse.next();
+  }
 
-  if (isAuthPage && user) {
+  // Logged in as normal user, but trying to access admin routes
+  if (isAdminRoute && user.role !== "admin") {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  if (isHomePage && !user) {
-    return NextResponse.redirect(new URL("/login", req.url));
+  // Logged in as admin, but trying to access the normal home page
+  if (isHomePage && user.role === "admin") {
+    return NextResponse.redirect(new URL("/admin", req.url));
+  }
+
+  // Already logged in? Don't let them see login/signup again.
+  if (isAuthPage) {
+    return NextResponse.redirect(
+      new URL(user.role === "admin" ? "/admin" : "/", req.url)
+    );
   }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/login", "/signup"],
-
+  matcher: ["/", "/login", "/signup", "/admin/:path*"],
 };
